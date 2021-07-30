16b55bd0a7b6c8bb106b46a115999b344708fa4a
Abdulaziz Almohammadi <AbdulazizAlmohammadi@users.noreply.github.com>
2021-07-30 16:25:21 +0300

    add footer and complete storys screen

--- snap/lib/screens/Storys.js
+++ snap/lib/screens/Storys.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 import AppBar from "../components/AppBar";
 import SectionTitle from "../components/SectionTitle";
 import StoryCard from "../components/StoryCard";
+import Feather from "react-native-vector-icons/Feather";
 
+const windowWidth = Dimensions.get("window").width;
 export default function Storys() {
   const names = [
     "Ezz",
@@ -59,7 +68,13 @@ export default function Storys() {
             <StoryCard name="Mazen" />
           </ScrollView>
         </View>
+        <View style={styles.scrollEnd}></View>
       </ScrollView>
+      <View style={styles.footer}>
+        <Feather name="message-square" size={25} color="gray"></Feather>
+        <Feather name="circle" size={70} color="gray"></Feather>
+        <Feather name="more-horizontal" size={30} color="gray"></Feather>
+      </View>
     </View>
   );
 }
@@ -74,8 +89,22 @@ const styles = StyleSheet.create({
     flex: 2,
     borderTopRightRadius: 20,
     borderTopLeftRadius: 20,
+    paddingBottom: 100,
+  },
+  scrollEnd: {
+    height: 100,
   },
   section: {
     flex: 1,
+  },
+  footer: {
+    height: 90,
+    width: windowWidth,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    position: "absolute",
+    bottom: 0,
   },
 });
