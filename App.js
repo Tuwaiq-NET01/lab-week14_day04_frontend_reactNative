a0bf9141a6a43898fe58c7134311749a1b444248
Mohammed Rashed <Mohammed.Albalawi@tuwaiq.edu.sa>
2021-07-30 16:40:12 +0300

    hw

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import Fontisto from 'react-native-vector-icons/Fontisto'
+import Footer from './Footer';
+import Main from './Main';
 
 export default function App() {
   return (
@@ -15,15 +17,12 @@ export default function App() {
         <Ionicons name='person-add' size={26  } color='white'/>
       </View>
 
-      <View style={styles.other}>
-
+      <View style={styles.main}>
+        <Text style={{fontWeight: 'bold', fontSize: 20, justifyContent: 'flex-start'}}>Friends <Fontisto name="angle-right" size={20} color="gray" /> </Text>
+        <Main />
       </View>
 
-      <View style={styles.footer}>
-        <Feather name="message-square" size={25} color="gray" style={{alignSelf: 'flex-end', marginBottom: 10, paddingRight: 20}} />
-        <Feather name="circle" size={70} color="gray" />
-        <Fontisto name="snapchat" size={25} color="gray" style={{alignSelf: 'flex-end', marginBottom: 10, paddingLeft: 20}} />
-      </View>
+      <Footer />
       
     </View>
   );
@@ -44,18 +43,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#9154bb',
   },
-  other: {
+  main: {
     flex:9,
-    backgroundColor: 'red',
+    
   },
-  footer: {
-    height: 90,
-    width: '100%',
-    flexDirection: "row", 
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(255, 255, 255, 0.8)",
-    position: "absolute",
-    bottom: 0
-  }
 });
