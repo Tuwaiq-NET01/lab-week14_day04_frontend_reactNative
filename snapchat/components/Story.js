c289d76a807b9ac42dbb194ec293b2373945caad
AfraAllahyani <Afra.Allahyani@tuwaiq.edu.sa>
2021-07-30 19:47:04 +0300

    snapchat lab

---
+++ snapchat/components/Story.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet, Image} from 'react-native'
+export default class Story extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOpen: false
+        }
+    }
+    render() {
+        return (
+            <View>
+                <View style={styles.continer} >
+                        <Image style={styles.image} source={{ uri: "https://picsum.photos/200/300" }}></Image>
+                        <Text style={styles.username}>{this.props.username}</Text>
+                </View>
+            </View >
+        )
+    }
+}
+const styles = StyleSheet.create({
+    continer: {
+        width: 100,
+        height: 170
+    },
+    image: {
+        flex: 1,
+        justifyContent: "center",
+        borderRadius:10,
+        
+        
+
+    },
+    username: {
+        color: "black",
+        alignSelf: "center",
+        fontWeight:"bold",
+        marginTop: 2,
+        fontSize:12
+    }
+})
