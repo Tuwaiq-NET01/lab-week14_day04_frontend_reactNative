c79a8c731c189064b6a96c9ea2475e5d7eca1291
teefeid <taef_415@hotmail.com>
2021-07-30 17:05:38 +0300

    Decompression

---
+++ snapchat/components/Trend.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet, Image } from 'react-native'
+export default class Trend extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOpen: false
+        }
+    }
+    render() {
+        return (
+            <View>
+                <View style={styles.continer} resizeMode="cover">
+                    <Image style={styles.image} source={{ uri: "https://picsum.photos/200/300" }}></Image>
+                    <Text style={styles.username}>{this.props.username}</Text>
+                </View>
+            </View >
+        )
+    }
+}
+const styles = StyleSheet.create({
+    continer: {
+        width: 190,
+        height: 290
+        
+    },
+
+    image: {
+        width: 40,
+        height: 40,
+    },
+    image: {
+        flex: 1,
+        borderRadius:12,
+    },
+    username: {
+        color: "white",
+        alignSelf: "center",
+        fontWeight:"bold",
+        fontSize:14,
+        backgroundColor:"gray",
+    }
+})
